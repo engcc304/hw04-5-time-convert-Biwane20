39b6dd6412157e4108a5d75ccbfecd64ba57c604
Biwane20 <biwbew20@gmail.com>
2023-08-16 22:57:01 +0700

    HW04-5

--- Homework/HW04/hw04-5-time-convert.cpp
+++ Homework/HW04/hw04-5-time-convert.cpp
@@ -1,15 +1,34 @@
 /*
     จงเขียนโปรแกรมเพื่อแปลงหน่วยจาก "วัน" เป็น "วินาที"
-    
+
     Test case:
-        Input Days : 
+        Input Days :
             8
     Output:
         8 days = 691200 seconds
 
     Test case:
-        Input Days : 
+        Input Days :
             27
     Output:
         27 days = 2332800 seconds
 */
+#include <stdio.h>
+#include <locale.h>
+int main()
+{
+    char ID[10];
+    int Work_hrs = 0;
+    float Salary = 0;
+    printf("Please Enter your ID : ");
+    scanf("%s", ID);
+    printf("Please Enter your working(hrs) : ");
+    scanf("%d", &Work_hrs);
+    printf("Please Enter your Salary : ");
+    scanf("%f", &Salary);
+    setlocale(LC_NUMERIC, "");
+    printf("Expected Output:\n");
+    printf("Employees ID = %s\n", ID);
+    printf("Salary = U$ %'.2f", Salary * Work_hrs);
+    return 0;
+}
